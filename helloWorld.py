704fde3f693b0154cfdd424fc3eb641ed62e94e9
ikeecode <ikeecode@gmail.com>
2023-10-04 05:27:19 +0000

    Add the for loop logic to branch earth

--- helloWorld.py
+++ helloWorld.py
@@ -2,3 +2,6 @@ print('this is the first commit')
 print('You are learning so be focus and determined')
 print('This is earth')
 
+for i in range(20):
+    if i%2==0:
+        print('Tu es libre de tes mouvements')
