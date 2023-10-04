eb01b4ba8d145213b65cd9014d931ae17d81a229
imKaba <mahamadou.kaba@meltwater.org>
2023-10-04 09:05:03 +0000

    Elon musk is one of a kind

--- helloWorld.py
+++ helloWorld.py
@@ -2,3 +2,4 @@ print('this is the first commit')
 print('You are learning so be focus and determined')
 print('This is Mars, if you breath you are already dead')
 
+print('Elon Musk wants to establish a colony in mars')
